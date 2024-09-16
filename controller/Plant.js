617dc991ccc359f94baafbba7ca5fe9a6a4d3468
Ansharg <anshgarg94161@gmail.com>
2024-09-16 17:31:18 +0530

    add plant issue fixed

--- controller/Plant.js
+++ controller/Plant.js
@@ -2,12 +2,11 @@ const User = require("../model/User");
 
 module.exports.DisplayAllPlants = async (req, res) => {
   try {
-    console.log(req.session.user, req.session.isLoggedIn);
-    let plants = await User.find({ id: req.session.user._id });
-    if (!plants) {
-      res.status(404).json({ message: "No plants found in your garden" });
+    let {garden} = await User.find({ id: req.body.id });
+    if (garden.length == 0) {
+      res.status(200).json({ message: "No plants found in your garden" });
     }
-    res.status(200).json({ plants: plants.garden });
+    res.status(200).json({ plants: garden });
   } catch (error) {
     res.status(400).json({ message: "Something went wrong" });
   }
@@ -15,9 +14,9 @@ module.exports.DisplayAllPlants = async (req, res) => {
 
 module.exports.AddPlant = async (req, res) => {
   try {
-    const { imgUrl, name } = req.body;
+    const { imgUrl, name, id } = req.body;
     const plant = new Object({ imgUrl: imgUrl, name: name });
-    let user = await User.findById(req.session.user._id);
+    let user = await User.findById(id);
     if (!user) {
       res.status(404).json({message: "User not found" });
     }
@@ -35,7 +34,7 @@ module.exports.DeletePlant = async (req, res) => {
     let { garden } = await User.findById(req.session.user._id);
     const index = req.params.index;
     garden.splice(index, 1)
-    await User.findByIdAndUpdate(req.session.user._id, {
+    await User.findByIdAndUpdate(req.body.id, {
       garden: garden,
     });
     res.status(200).json({ message: "Plant deleted successfully" });
@@ -47,7 +46,7 @@ module.exports.DeletePlant = async (req, res) => {
 
 module.exports.startGrowing = async (req, res) => {
   try {
-    let { garden } = await User.findById(req.session.user._id);
+    let { garden } = await User.findById(req.body.id);
     const index = req.params.index;
     const today = new Date();
     const yyyy = today.getFullYear();
@@ -63,7 +62,7 @@ module.exports.startGrowing = async (req, res) => {
       plantedDate: formattedToday,
     });
     garden[index] = growing;
-    await User.findByIdAndUpdate(req.session.user._id, { garden: garden });
+    await User.findByIdAndUpdate(req.body.id, { garden: garden });
     res.status(200).json({ message: "Plant started growing" });
   } catch (error) {
     console.log(error);
